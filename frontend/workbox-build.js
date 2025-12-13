971e6f891e089d1544a64485d0862bde4e3563b2
mohamedbaatour <mohamedbaatourx@gmail.com>
2025-12-13 22:20:05 +0100

    trying to fix offline app not working V1

--- frontend/workbox-build.js
+++ frontend/workbox-build.js
@@ -1,8 +1,14 @@
 const workboxBuild = require("workbox-build");
 
-workboxBuild.injectManifest({
-  swSrc: "public/sw-src.js",
-  swDest: "build/sw.js",
+workboxBuild.generateSW({
   globDirectory: "build",
   globPatterns: ["**/*.{html,js,css,svg,png,ico,json,webp,avif,woff2}"],
+  swDest: "build/sw.js",
+  skipWaiting: true,
+  clientsClaim: true,
+  cleanupOutdatedCaches: true,
+
+  // SPA offline fallback
+  navigateFallback: "/index.html",
+  navigateFallbackDenylist: [/^\/api\//],
 }).then(() => console.log("✅ SW generated at build/sw.js"));
